82001e257e4d775d7212bf7df4bd443788d2704f
Stefan Stax <stefanstaxbusiness@gmail.com>
2023-06-22 20:52:16 +0200

    Redux setup and finalization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import MoviePlaylist from "./components/MoviePlaylist";
+import SongPlaylist from "./components/SongPlaylist";
+import { useDispatch } from "react-redux";
+import { reset } from "./store/actions";
 
-function App() {
+export default function App() {
+  const dispatch = useDispatch();
+  const handleResetClick = () => {
+    dispatch(reset());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container is-fluid">
+      <button onClick={() => handleResetClick()} className="button is-danger">
+        Reset Both Playlists
+      </button>
+      <hr />
+      <MoviePlaylist />
+      <hr />
+      <SongPlaylist />
     </div>
   );
 }
-
-export default App;
